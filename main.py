bb80dd639fa9ab5b6968d2cd5debcbf03c46b7b1
SONG JUNBONG <junbong.song@gmail.com>
2021-01-16 03:04:54 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+
+while(True):
+  a=int(input("숫자를 입력하세요:"))
+  if (a==1):
+    print("a는 1입니다")
+    break
+  else:
+    print("a는 1이 아닙니다")
+
+
+count=0
+while (count<10):
+  print("무한반복",count,"번째")
+  count=count+1
+
+count=0
+while(True):
+  print("무한반복") 
+  count=count+1
+  if(count==5):
+    break
+
